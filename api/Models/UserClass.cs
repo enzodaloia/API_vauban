1305908e9c4dae1ef8e3e9791bfa28697e0c9d31
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2024-05-15 14:27:15 +0200

    role

--- api/Models/UserClass.cs
+++ api/Models/UserClass.cs
@@ -7,6 +7,7 @@ namespace API.Models
         public string email { get; set; }
         public string name { get; set; }
         public string password { get; set; }
+        public string role { get; set; }
     }
 }
 
